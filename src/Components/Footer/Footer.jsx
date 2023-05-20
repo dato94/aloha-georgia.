cd02d7c7308dd83ea03738dda6e1e939508dfc87
datogithub <104518080+datogithub@users.noreply.github.com>
2023-05-19 18:41:21 -0700

    add: footer, contact.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,163 @@
 import React from "react";
-import "./footer.css";
+import "./footer.scss";
+import video2 from "../../assets/video2.mp4";
+import {FiSend} from "react-icons/fi"
+import {MdOutlineTravelExplore} from "react-icons/md"
+import {AiOutlineTwitter} from "react-icons/ai"
+import {AiFillYoutube} from "react-icons/ai"
+import {AiFillInstagram} from "react-icons/ai"
+import {FaTripadvisor} from "react-icons/fa"
+import {FiChevronRight} from "react-icons/fi"
+
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <section className="footer">
+            <div className="videoDiv">
+                <video src={video2} loop autoPlay muted type="video/mp4"></video>
+            </div>
+
+            <div className="setContent container">
+                <div className="contactDiv flex">
+                    <div className="text">
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel with us</h2>
+                    </div>
+
+                    <div className="inputDiv flex">
+                        <input type="text" placeholder="Enter Email Address" />
+                        <button className="btn flex" type="submit">
+                            SEND <FiSend className="icon" />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="footerCard flex">
+                    <div className="footerIntro flex">
+                        <div className="logoDiv">
+                            <a href="#" className="logo flex">
+                             <MdOutlineTravelExplore className="icon"/> Travel.
+                            </a>
+                        </div>
+
+                        <div className="footerParagraph">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, repudiandae! Rerum incidunt sunt modi nesciunt rem cumque aperiam labore repellendus reprehenderit nostrum repudiandae odio, dignissimos eaque. Non dignissimos magni iusto!
+                        </div>
+
+                        <div className="footerSocials flex">
+                            <AiOutlineTwitter className="icon"/>
+                            <AiFillYoutube className="icon" />
+                            <AiFillInstagram className="icon" />
+                            <FaTripadvisor className="icon" />
+                        </div>
+                    </div>
+                
+                    <div className="footerLinks grid">
+          {/* Group One */}
+                        <div className="linkGroup">
+                            <span className="groupTitle">
+                                OUR AGENCY
+                            </span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Services
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Insurance
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Agency
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Tourism
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Payment
+                            </li>
+
+                        </div>
+          {/* Group Two */}
+                         <div className="linkGroup">
+                            <span className="groupTitle">
+                                PARTNERS
+                            </span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Bookings
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                RentCars
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                HostelWorld
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                TripAgo
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                TripAdvisor
+                            </li>
+
+                        </div>
+             {/* Group Three */}
+                         <div className="linkGroup">
+                            <span className="groupTitle">
+                                LAST MINUTE
+                            </span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                London
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                California
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Indonesia
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Europe
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Oceania
+                            </li>
+
+                        </div>
+                    </div>
+
+                    <div className="footerDiv flex">
+                        <small>BEST TRAVEL WEBSITE THEME</small>
+                        <a href="https://mtebi.com/" target={"_blank"}>@Mtebi</a>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
